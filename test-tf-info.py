97598655b884054943d876d83f3023323bbaca34
rguillome <dev@guillaumerenaudin.com>
2019-05-13 17:02:11 +0200

    add device listing

--- test-tf-info.py
+++ test-tf-info.py
@@ -1,4 +1,8 @@
 import tensorflow as tf
+
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
+
 hello = tf.constant('Hello, TensorFlow!')
 sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 print(sess.run(hello))
